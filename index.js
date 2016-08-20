a70a9d04555bf5d004ac4951d2f502c6ebb373c2
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-08-20 15:08:23 -0700

    midi code

--- index.js
+++ index.js
@@ -12,3 +12,33 @@ createSynth({
   }
   `
 }).connect(audioContext.destination)
+
+//----------------------------
+var wsock = require('websocket-stream')
+var to = require('to2')
+var split = require('split2')
+var onend = require('end-of-stream')
+
+var kmin = 48, kmax = 72
+var state = { time: 0, keys: [] }
+
+for (var i = kmin; i < kmax; i++) {
+  state.keys.push(0)
+}
+
+;(function recon () {
+  var stream = wsock('ws://localhost:5000')
+  onend(stream, recon)
+  stream.pipe(split(JSON.parse))
+    .pipe(to.obj(write))
+  return recon
+
+  function write (row, enc, next) {
+    state.time += row.dt
+    var keydown = (row.values[0]>>4)&1
+    var k = row.values[1] - kmin
+    state.keys[k] = keydown * row.values[2]/128
+    console.log(state.keys.join(','))
+    next()
+  }
+})()
---
+++ server.js
@@ -0,0 +1,29 @@
+var wsock = require('websocket-stream')
+var onend = require('end-of-stream')
+var http = require('http')
+
+var midi = require('midi')
+var input = new midi.input()
+
+var server = http.createServer(function (req, res) {
+  res.statusCode = 404
+  res.end('not found\n')
+})
+server.listen(5000)
+
+var streams = []
+wsock.createServer({ server: server }, function (stream) {
+  streams.push(stream)
+  onend(stream, function () {
+    var ix = streams.indexOf(stream)
+    streams.splice(ix, 1)
+  })
+})
+
+input.on('message', function (dt, msg) {
+  for (var i = 0; i < streams.length; i++) {
+    streams[i].write(JSON.stringify({ dt: dt, values: msg })+'\n')
+  }
+  console.log({ dt: dt, values: msg })
+})
+input.openPort(1)
