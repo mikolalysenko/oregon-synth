5806612d6a14957c832bd7ca4a48789765bef088
mikolalysenko <mikolalysenko@gmail.com>
2016-08-25 21:30:31 -0600

    take out column for now

--- gfx.js
+++ gfx.js
@@ -3,11 +3,11 @@ const MOVIE_TIME = 10e4
 module.exports = function (args) {
   const regl = args.regl
   const visuals = [
-    require('./visuals/columns')(args),
+    // require('./visuals/columns')(args),
     require('./visuals/sph')(args)
   ]
 
-  let activeVisual = visuals[1]
+  let activeVisual = visuals[0]
   let visualTime = MOVIE_TIME
   regl.frame(({tick}) => {
     if (--visualTime < 0) {
