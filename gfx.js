dc8e86032b32cde9ded8cf3351e2ebf1a379f3f2
mikolalysenko <mikolalysenko@gmail.com>
2016-08-25 21:06:43 -0600

    add column

--- gfx.js
+++ gfx.js
@@ -1,18 +1,19 @@
-const MOVIE_TIME = 30e4
+const MOVIE_TIME = 10e4
 
 module.exports = function (args) {
   const regl = args.regl
   const visuals = [
-    require('./visuals/sph.js')(args)
+    require('./visuals/columns')(args),
+    require('./visuals/sph')(args)
   ]
 
-  let activeVisual = 0
+  let activeVisual = visuals[0]
   let visualTime = MOVIE_TIME
   regl.frame(({tick}) => {
     if (--visualTime < 0) {
       visualTime = MOVIE_TIME
       activeVisual = visuals[(Math.random() * visuals.length) | 0]
     }
-    (visuals[activeVisual])()
+    activeVisual()
   })
 }
